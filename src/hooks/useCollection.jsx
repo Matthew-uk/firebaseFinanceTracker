63b55d9072199d988d5cbf503abb06299578512f
Matthew-uk <esaduviedede@gmail.com>
2023-03-30 11:42:56 +0100

    97% done...

--- src/hooks/useCollection.jsx
+++ src/hooks/useCollection.jsx
@@ -4,25 +4,32 @@ import { projectFireStore } from "../firebase/config";
 export const useCollection = (collection) => {
   const [docs, setDocs] = useState([]);
   const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
 
   useEffect(() => {
     const ref = projectFireStore.collection(collection);
 
-    const unsubcribe = ref.onSnapshot(
-      (snapShot) => {
-        const data = snapShot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        setDocs(data);
-      },
-      (err) => {
-        setError(err);
-        console.log(`This is error from use Collection: ${error}`);
-      }
-    );
-    return unsubcribe;
+    const unsubcribe = () => {
+      setIsPending(true);
+      setError(null);
+      ref.onSnapshot(
+        (snapShot) => {
+          setIsPending(true);
+          const data = snapShot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }));
+          setDocs(data);
+          setIsPending(false);
+        },
+        (err) => {
+          setError(err);
+          console.log("This is error from use Collection: ", err);
+        }
+      );
+    };
+    return unsubcribe();
   }, [collection]);
 
-  return { docs };
+  return { docs, isPending };
 };
--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -4,34 +4,40 @@ import TransactionList from "../../components/tList/tList";
 import Transaction from "../../components/transaction/transaction";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { useCollection } from "../../hooks/useCollection";
+import { Loading } from "./../../components/loading/loading";
 import "./dashboard.css";
 
 const Dashboard = () => {
   const { user } = useAuthContext();
-  const { docs } = useCollection("Transactions");
+  const { docs, isPending } = useCollection("Transactions");
   const filteredDocs = docs.filter((doc) => doc.uid === user.uid);
 
-  return (
-    <div className="dashboard">
-      <Nav />
-      <div className="container">
-        <div className="transactionList">
-          <div className="transactionListBox">
-            {
-              docs &&
-                filteredDocs.map((doc) => (
-                  <TransactionList key={doc.id} doc={doc} />
-                ))
-              // docs.map((doc) => <TransactionList key={doc.id} doc={doc} />)
-            }
+  const DashboardBody = () => {
+    return (
+      <div className="dashboard">
+        <Nav />
+        <div className="container">
+          <div className="transactionList">
+            <div className="transactionListBox">
+              {
+                docs
+                  ? filteredDocs.map((doc) => (
+                      <TransactionList key={doc.id} doc={doc} />
+                    ))
+                  : ""
+                // docs.map((doc) => <TransactionList key={doc.id} doc={doc} />)
+              }
+            </div>
+          </div>
+          <div className="transactionForm">
+            <Transaction user={user} />
           </div>
         </div>
-        <div className="transactionForm">
-          <Transaction user={user} />
-        </div>
       </div>
-    </div>
-  );
+    );
+  };
+
+  return <div>{isPending ? <Loading /> : <DashboardBody />}</div>;
 };
 
 export default Dashboard;
